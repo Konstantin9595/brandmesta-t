90ec36c6dddd56574268b55ab4ebb8264fa70026
konstantins9595 <konstantins9595@gmail.com>
2023-07-27 19:16:21 +0600

    Add hover button animation

--- client/src/app/components/HeaderRightDynamic.tsx
+++ client/src/app/components/HeaderRightDynamic.tsx
@@ -2,6 +2,7 @@
 import { FC, PropsWithChildren, useState } from "react";
 import MobileMenuButton from "./MobileMenuButton";
 import Button from "./Button";
+import CallToActionButton from "./CallToActionButton";
 
 const HeaderRightDynamic: FC<PropsWithChildren> = ({ children }) => {
   const [isOpen, setOpen] = useState(false);
